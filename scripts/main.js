cec576d01defb71c504bb11bd600b389b892346c
Andrew Meng <andrewmeng810@gmail.com>
2020-02-06 11:23:19 +0800

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,39 @@
+// let myHeading = document.querySelector('h1');
+// myHeading.textContent = 'Hello World!';
+
+/*
+let myVariable = document.querySelector('h1');
+以及上面所有示例都是对象。
+*/
+let myImage = document.querySelector('img');
+myImage.onclick = function() {
+    let mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/test_img1.png') {
+      myImage.setAttribute('src', 'images/test_img2.png');
+    } else {
+      myImage.setAttribute('src', 'images/test_img1.png');
+    }
+}
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName() {
+  let myName = prompt('请输入你的名字。');
+  if(!myName || myName === null) {
+    setUserName();
+  } else {
+    localStorage.setItem('name', myName);
+    myHeading.innerHTML = 'Mozilla 酷毙了，' + myName;
+  }
+}
+// 初始化代码，因为它在页面初次读取时进行构造工作
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  let storedName = localStorage.getItem('name');
+  myHeading.textContent = 'Mozilla 酷毙了，' + storedName;
+}
+// 按钮设置 onclick 事件处理器
+myButton.onclick = function() {
+   setUserName();
+}
